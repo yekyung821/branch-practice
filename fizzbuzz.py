a052dd416e2371619f1e5a72e564f0320f1d9510
yekyung821 <bubblekid43@gmail.com>
2025-08-22 12:33:14 +0900

    feat: Add FizzBuzz Code

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,9 +1,9 @@
 for i in range(1,15+1):
-    if i % 3 == 0 :
+    if i % 15 == 0 :
+        print('FizzBuzz')
+    elif i % 3 == 0 :
         print('Fizz')
     elif i % 5 == 0 :
         print('Buzz')
-    elif i % 15 == 0 :
-        print('FizzBuzz')
     else :
         print(i)
